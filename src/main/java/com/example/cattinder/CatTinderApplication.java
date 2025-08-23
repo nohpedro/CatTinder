e64b8780b88066dfc226d8dcda458626f3e55a54
Mr Pedro Santos <147351155+nohpedro@users.noreply.github.com>
2025-08-23 19:41:21 -0400

    INitital Commit

---
+++ src/main/java/com/example/cattinder/CatTinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.cattinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatTinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatTinderApplication.class, args);
+    }
+
+}
