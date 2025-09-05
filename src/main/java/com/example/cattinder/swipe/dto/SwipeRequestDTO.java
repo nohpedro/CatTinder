b0ea112f1936ce7decbab60162cb21b50f937617
Mr Pedro Santos <147351155+nohpedro@users.noreply.github.com>
2025-09-05 18:38:50 -0400

    Upgrade

--- src/main/java/com/example/cattinder/swipe/dto/SwipeRequestDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.cattinder.swipe.dto;
-
-public class SwipeRequestDTO {
-    private String dir;
-    public String getDir() {return dir;}
-    public void setDir(String dir) {this.dir = dir;}
-    }
