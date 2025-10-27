3673aecc8c39dddb7b242ed6dc53ec046c1cbc92
ElNegis <128728636+ElNegis@users.noreply.github.com>
2025-10-27 19:00:48 -0400

    a
    a

---
+++ confg-ms/src/main/java/com/example/confgms/excepcion/Error/ExcepcionSolicitudInvalida.java
@@ -0,0 +1,7 @@
+package com.example.confgms.excepcion.Error;
+
+public class ExcepcionSolicitudInvalida extends RuntimeException {
+    public ExcepcionSolicitudInvalida(String message) {
+        super(message);
+    }
+}
