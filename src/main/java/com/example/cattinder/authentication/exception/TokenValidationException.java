87b56aeae134827e08c0b9e2b0a0ff64da424285
Tu Nombre de Usuario <tu-correo@ejemplo.com>
2025-12-15 11:30:41 -0400

    corregido

--- src/main/java/com/example/cattinder/authentication/exception/TokenValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cattinder.authentication.exception;
-
-public class TokenValidationException extends RuntimeException {
-    public TokenValidationException(String message) {
-        super(message);
-    }
-}
